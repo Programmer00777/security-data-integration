6722382bb3f9d283b4e962973708f0c182a93b75
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2023-01-05 09:36:30 +0200

    initial commit

---
+++ src/main/java/com/codelib/securitydataintegration/SecurityDataIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.codelib.securitydataintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDataIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDataIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codelib/securitydataintegration/SecurityDataIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelib.securitydataintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDataIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
